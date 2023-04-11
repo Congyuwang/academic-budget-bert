9add6d7851d7772672a6a6f57f8d562b29cc4515
root <leonwang998@foxmail.com>
2023-04-11 14:25:44 +0800

    use data_processing.py

--- scripts/extract_wiki.sh
+++ scripts/extract_wiki.sh
@@ -2,4 +2,4 @@ pip install wikiextractor
 IN_PATH=../wikidata/enwiki-20230401-pages-articles.xml.bz2
 OUT_PATH=../wikidata/extract
 mkdir -p $OUT_PATH
-wikiextractor -o $OUT_PATH --processes 30 $IN_PATH
+python ../dataset/process_data.py -f $IN_PATH --n_processes 30 -o $OUT_PATH --type wiki
